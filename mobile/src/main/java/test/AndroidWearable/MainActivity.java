44ed654c3928dcb53bde540bc1c5972a90233681
PrabhanshuShekhar <prabhanshu@motifworks.com>
2015-10-07 11:13:38 +0530

    commit to test data sync between handheld to wearable

--- mobile/src/main/java/test/AndroidWearable/MainActivity.java
+++ mobile/src/main/java/test/AndroidWearable/MainActivity.java
@@ -1,17 +1,86 @@
 package test.AndroidWearable;
 
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements DataApi.DataListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    ArcProgress arcProgress;
+    ImageView alarmIV;
+    Timer timer, timerDweetPost;
+    double currentTemp;
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        arcProgress = (ArcProgress)findViewById(R.id.arc_progress);
+        alarmIV = ( ImageView)findViewById(R.id.alarmIV);
+        arcProgress.setStrokeWidth(40);
+        arcProgress.setUnfinishedStrokeColor(Color.parseColor("#A9A9A9"));
+        arcProgress.setTextSize(40);
+        arcProgress.setBackgroundColor(Color.TRANSPARENT);
+        mGoogleApiClient  = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+
+        timerDweetPost = new Timer();
+        timerDweetPost.scheduleAtFixedRate( new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        System.out.println(">>>>>> scheduler");
+                        new DweetPostRequest().execute(null);
+                    }
+                });
+            }
+        },1000,60*1000);
     }
 
 
@@ -36,4 +105,170 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+
+
+
+    private class DweetPostRequest extends AsyncTask
+    {
+        @Override
+        protected Object doInBackground(Object[] params) {
+            try {
+                double lower = -90.101;
+                double upper = -49.3098;
+                double result = Math.random() * (upper - lower) + lower;
+                DefaultHttpClient httpClient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost("https://dweet.io:443/dweet/for/freezerTemp");
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("temp",result);
+                if (jsonObject != null) {
+                    StringEntity stringEntity = new StringEntity(
+                            jsonObject.toString());
+                    stringEntity.setContentType(new BasicHeader(HTTP.CONTENT_TYPE,
+                            "application/json"));
+                    httpPost.setEntity(stringEntity);
+                }
+                HttpResponse httpResponse = httpClient.execute(httpPost);
+                HttpEntity httpEntity = httpResponse.getEntity();
+                InputStream is = httpEntity.getContent();
+                System.out.println(">>>>>Dweet post response:"+is.toString());
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            new DweetGetRequest().execute(null);
+        }
+    }
+
+
+    private class DweetGetRequest extends AsyncTask
+    {
+        @Override
+        protected Object doInBackground(Object[] params) {
+            // https://dweet.io:443/get/latest/dweet/for/freezerTemp
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpGet httpGet = new HttpGet("https://dweet.io:443/get/latest/dweet/for/freezerTemp");
+            HttpResponse httpResponse;
+            try {
+                httpResponse = httpClient.execute(httpGet);
+                HttpEntity httpEntity = httpResponse.getEntity();
+                InputStream is = httpEntity.getContent();
+                String jsonString = "";
+                JSONArray jsonArray = null;
+                try {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(
+                            is, "iso-8859-1"), 8);
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line + "\n");
+                    }
+                    is.close();
+                    jsonString = sb.toString();
+                } catch (Exception e) {
+                }
+                System.out.println(">>>>> get response:" + jsonString.toString());
+                JSONObject jsonObject = new JSONObject(jsonString);
+                System.out.println(">>>>> temp:" + jsonObject.getJSONArray("with").getJSONObject(0).getJSONObject("content").getDouble("temp"));
+                currentTemp = jsonObject.getJSONArray("with").getJSONObject(0).getJSONObject("content").getDouble("temp");
+
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            updateTemperature();
+            arcProgress.setProgress(0);
+            System.out.println(">>>>>> currentTemp :"+currentTemp);
+            if(currentTemp >= -85.00 && currentTemp <= -75.00)
+            {
+                System.out.println(">>>>> green color");
+                arcProgress.setFinishedStrokeColor(getResources().getColor(R.color.green_progress_color));
+                arcProgress.setTextColor(getResources().getColor(R.color.green_temperature_color_code));
+                alarmIV.setVisibility(View.INVISIBLE);
+            }else if(currentTemp >= -75.00 && currentTemp <= -65.00)
+            {
+                System.out.println(">>>>>> yellow color");
+                arcProgress.setFinishedStrokeColor(getResources().getColor(R.color.yellow_progress_color));
+                arcProgress.setTextColor(getResources().getColor(R.color.yellow_temperature_color_code));
+                alarmIV.setImageResource(R.drawable.alert);
+            }else if(currentTemp >= -65.00 && currentTemp <= -50.00)
+            {
+                System.out.println(">>>>> red color");
+                arcProgress.setFinishedStrokeColor(getResources().getColor(R.color.red_progress_color));
+                arcProgress.setTextColor(getResources().getColor(R.color.red_temperature_color_code));
+                alarmIV.setImageResource(R.drawable.fire_alarm);
+            }
+
+            timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if(arcProgress.getProgress() < Math.abs(currentTemp))
+                                arcProgress.setProgress(arcProgress.getProgress() + 1);
+                            else
+                                timer.cancel();
+                        }
+                    });
+                }
+            }, 1000, 100);
+        }
+    }
+
+
+  void updateTemperature()
+  {
+      PutDataMapRequest putDataMapReq = PutDataMapRequest.create("/temp");
+      putDataMapReq.getDataMap().putDouble("currentTemp", currentTemp);
+      PutDataRequest putDataReq = putDataMapReq.asPutDataRequest();
+      PendingResult<DataApi.DataItemResult> pendingResult =
+              Wearable.DataApi.putDataItem(mGoogleApiClient, putDataReq);
+  }
+
+
+
+
 }
--- wear/src/main/java/test/AndroidWearable/MainActivity.java
+++ wear/src/main/java/test/AndroidWearable/MainActivity.java
@@ -2,58 +2,70 @@ package test.AndroidWearable;
 
 import android.app.Activity;
 import android.graphics.Color;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.view.View;
 import android.widget.ImageView;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.protocol.HTTP;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.Wearable;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements
+        DataApi.DataListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
     Timer timer, timerDweetPost;
     ImageView alarmIV;
     double currentTemp;
     ArcProgress arcProgress;
+    GoogleApiClient mGoogleApiClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        timerDweetPost = new Timer();
-        timerDweetPost.scheduleAtFixedRate( new TimerTask() {
-            @Override
-            public void run() {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
 
-                        System.out.println(">>>>>> scheduler");
-                        new DweetPostRequest().execute(null);
-                    }
-                });
-            }
-        },1000,60*1000);
+
+        mGoogleApiClient  = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+
+
+
+
+
+
+
+
+//        timerDweetPost = new Timer();
+//        timerDweetPost.scheduleAtFixedRate( new TimerTask() {
+//            @Override
+//            public void run() {
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//
+//                        System.out.println(">>>>>> scheduler");
+//                        new DweetPostRequest().execute(null);
+//                    }
+//                });
+//            }
+//        },1000,60*1000);
 
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
@@ -90,134 +102,185 @@ public class MainActivity extends Activity {
 
 
 
-    private class DweetPostRequest extends AsyncTask
-    {
-        @Override
-        protected Object doInBackground(Object[] params) {
-            try {
-                double lower = -90.101;
-                double upper = -49.3098;
-                double result = Math.random() * (upper - lower) + lower;
-                DefaultHttpClient httpClient = new DefaultHttpClient();
-                HttpPost httpPost = new HttpPost("https://dweet.io:443/dweet/for/freezerTemp");
-                JSONObject jsonObject = new JSONObject();
-                jsonObject.put("temp",result);
-                if (jsonObject != null) {
-                    StringEntity stringEntity = new StringEntity(
-                            jsonObject.toString());
-                    stringEntity.setContentType(new BasicHeader(HTTP.CONTENT_TYPE,
-                            "application/json"));
-                    httpPost.setEntity(stringEntity);
-                }
-                HttpResponse httpResponse = httpClient.execute(httpPost);
-                HttpEntity httpEntity = httpResponse.getEntity();
-                InputStream is = httpEntity.getContent();
-                System.out.println(">>>>>Dweet post response:"+is.toString());
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            } catch (ClientProtocolException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            catch (JSONException e)
-            {
-                e.printStackTrace();
-            }
-            return null;
-        }
+//    private class DweetPostRequest extends AsyncTask
+//    {
+//        @Override
+//        protected Object doInBackground(Object[] params) {
+//            try {
+//                double lower = -90.101;
+//                double upper = -49.3098;
+//                double result = Math.random() * (upper - lower) + lower;
+//                DefaultHttpClient httpClient = new DefaultHttpClient();
+//                HttpPost httpPost = new HttpPost("https://dweet.io:443/dweet/for/freezerTemp");
+//                JSONObject jsonObject = new JSONObject();
+//                jsonObject.put("temp",result);
+//                if (jsonObject != null) {
+//                    StringEntity stringEntity = new StringEntity(
+//                            jsonObject.toString());
+//                    stringEntity.setContentType(new BasicHeader(HTTP.CONTENT_TYPE,
+//                            "application/json"));
+//                    httpPost.setEntity(stringEntity);
+//                }
+//                HttpResponse httpResponse = httpClient.execute(httpPost);
+//                HttpEntity httpEntity = httpResponse.getEntity();
+//                InputStream is = httpEntity.getContent();
+//                System.out.println(">>>>>Dweet post response:"+is.toString());
+//            } catch (UnsupportedEncodingException e) {
+//                e.printStackTrace();
+//            } catch (ClientProtocolException e) {
+//                e.printStackTrace();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            catch (JSONException e)
+//            {
+//                e.printStackTrace();
+//            }
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(Object o) {
+//            super.onPostExecute(o);
+//            new DweetGetRequest().execute(null);
+//        }
+//    }
 
-        @Override
-        protected void onPostExecute(Object o) {
-            super.onPostExecute(o);
-            new DweetGetRequest().execute(null);
-        }
+
+//    private class DweetGetRequest extends AsyncTask
+//    {
+//        @Override
+//        protected Object doInBackground(Object[] params) {
+//            // https://dweet.io:443/get/latest/dweet/for/freezerTemp
+//            DefaultHttpClient httpClient = new DefaultHttpClient();
+//            HttpGet httpGet = new HttpGet("https://dweet.io:443/get/latest/dweet/for/freezerTemp");
+//            HttpResponse httpResponse;
+//            try {
+//                httpResponse = httpClient.execute(httpGet);
+//                HttpEntity httpEntity = httpResponse.getEntity();
+//                InputStream is = httpEntity.getContent();
+//                String jsonString = "";
+//                JSONArray jsonArray = null;
+//                try {
+//                    BufferedReader reader = new BufferedReader(new InputStreamReader(
+//                            is, "iso-8859-1"), 8);
+//                    StringBuilder sb = new StringBuilder();
+//                    String line = null;
+//                    while ((line = reader.readLine()) != null) {
+//                        sb.append(line + "\n");
+//                    }
+//                    is.close();
+//                    jsonString = sb.toString();
+//                } catch (Exception e) {
+//                }
+//                System.out.println(">>>>> get response:" + jsonString.toString());
+//                JSONObject jsonObject = new JSONObject(jsonString);
+//                System.out.println(">>>>> temp:" + jsonObject.getJSONArray("with").getJSONObject(0).getJSONObject("content").getDouble("temp"));
+//                currentTemp = jsonObject.getJSONArray("with").getJSONObject(0).getJSONObject("content").getDouble("temp");
+//
+//            } catch (Exception e)
+//            {
+//                e.printStackTrace();
+//            }
+//
+//
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(Object o) {
+//            super.onPostExecute(o);
+//            publishTemp();
+//
+//        }
+//    }
+
+public void  publishTemp()
+{
+    arcProgress.setProgress(0);
+    System.out.println(">>>>>> currentTemp :"+currentTemp);
+    if(currentTemp >= -85.00 && currentTemp <= -75.00)
+    {
+        System.out.println(">>>>> green color");
+        arcProgress.setFinishedStrokeColor(getResources().getColor(R.color.green_progress_color));
+        arcProgress.setTextColor(getResources().getColor(R.color.green_temperature_color_code));
+        alarmIV.setVisibility(View.INVISIBLE);
+    }else if(currentTemp >= -75.00 && currentTemp <= -65.00)
+    {
+        System.out.println(">>>>>> yellow color");
+        arcProgress.setFinishedStrokeColor(getResources().getColor(R.color.yellow_progress_color));
+        arcProgress.setTextColor(getResources().getColor(R.color.yellow_temperature_color_code));
+        alarmIV.setImageResource(R.drawable.alert);
+    }else if(currentTemp >= -65.00 && currentTemp <= -50.00)
+    {
+        System.out.println(">>>>> red color");
+        arcProgress.setFinishedStrokeColor(getResources().getColor(R.color.red_progress_color));
+        arcProgress.setTextColor(getResources().getColor(R.color.red_temperature_color_code));
+        alarmIV.setImageResource(R.drawable.fire_alarm);
     }
 
-
-    private class DweetGetRequest extends AsyncTask
-    {
+    timer = new Timer();
+    timer.schedule(new TimerTask() {
         @Override
-        protected Object doInBackground(Object[] params) {
-            // https://dweet.io:443/get/latest/dweet/for/freezerTemp
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpGet httpGet = new HttpGet("https://dweet.io:443/get/latest/dweet/for/freezerTemp");
-            HttpResponse httpResponse;
-            try {
-                httpResponse = httpClient.execute(httpGet);
-                HttpEntity httpEntity = httpResponse.getEntity();
-                InputStream is = httpEntity.getContent();
-                String jsonString = "";
-                JSONArray jsonArray = null;
-                try {
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(
-                            is, "iso-8859-1"), 8);
-                    StringBuilder sb = new StringBuilder();
-                    String line = null;
-                    while ((line = reader.readLine()) != null) {
-                        sb.append(line + "\n");
-                    }
-                    is.close();
-                    jsonString = sb.toString();
-                } catch (Exception e) {
-                }
-                System.out.println(">>>>> get response:" + jsonString.toString());
-                JSONObject jsonObject = new JSONObject(jsonString);
-                System.out.println(">>>>> temp:" + jsonObject.getJSONArray("with").getJSONObject(0).getJSONObject("content").getDouble("temp"));
-                currentTemp = jsonObject.getJSONArray("with").getJSONObject(0).getJSONObject("content").getDouble("temp");
-
-            } catch (Exception e)
-            {
-                e.printStackTrace();
-            }
-
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Object o) {
-            super.onPostExecute(o);
-            arcProgress.setProgress(0);
-            System.out.println(">>>>>> currentTemp :"+currentTemp);
-            if(currentTemp >= -85.00 && currentTemp <= -75.00)
-            {
-                System.out.println(">>>>> green color");
-                arcProgress.setFinishedStrokeColor(getResources().getColor(R.color.green_progress_color));
-                arcProgress.setTextColor(getResources().getColor(R.color.green_temperature_color_code));
-                alarmIV.setVisibility(View.INVISIBLE);
-            }else if(currentTemp >= -75.00 && currentTemp <= -65.00)
-            {
-                System.out.println(">>>>>> yellow color");
-                arcProgress.setFinishedStrokeColor(getResources().getColor(R.color.yellow_progress_color));
-                arcProgress.setTextColor(getResources().getColor(R.color.yellow_temperature_color_code));
-                alarmIV.setImageResource(R.drawable.alert);
-            }else if(currentTemp >= -65.00 && currentTemp <= -50.00)
-            {
-                System.out.println(">>>>> red color");
-                arcProgress.setFinishedStrokeColor(getResources().getColor(R.color.red_progress_color));
-                arcProgress.setTextColor(getResources().getColor(R.color.red_temperature_color_code));
-                alarmIV.setImageResource(R.drawable.fire_alarm);
-            }
-
-            timer = new Timer();
-            timer.schedule(new TimerTask() {
+        public void run() {
+            runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            if(arcProgress.getProgress() < Math.abs(currentTemp))
-                                arcProgress.setProgress(arcProgress.getProgress() + 1);
-                            else
-                                timer.cancel();
-                        }
-                    });
+                    if(arcProgress.getProgress() < Math.abs(currentTemp))
+                        arcProgress.setProgress(arcProgress.getProgress() + 1);
+                    else
+                        timer.cancel();
                 }
-            }, 1000, 100);
+            });
+        }
+    }, 1000, 100);
+}
+
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Wearable.DataApi.addListener(mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+        for (DataEvent event : dataEvents) {
+            if (event.getType() == DataEvent.TYPE_CHANGED) {
+                // DataItem changed
+                DataItem item = event.getDataItem();
+                if (item.getUri().getPath().compareTo("/temp") == 0) {
+                    DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
+                    currentTemp = dataMap.getDouble("currenttemp");
+                    publishTemp();
+//                    updateCount(dataMap.getInt(COUNT_KEY));
+                }
+            } else if (event.getType() == DataEvent.TYPE_DELETED) {
+                // DataItem deleted
+            }
         }
     }
 
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
 
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Wearable.DataApi.removeListener(mGoogleApiClient, this);
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onStart();
+        mGoogleApiClient.connect();
+
+    }
 }
